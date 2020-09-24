dc0fdbaf002b88b7edcd140eea6d596fa92cecb1
isprch <Ronald.J.Charest@qvc.com>
2020-09-24 12:35:43 -0400

    no message

--- src/main/java/com/qvc/mediatech/demo/config/ProductListConfig.java
+++ src/main/java/com/qvc/mediatech/demo/config/ProductListConfig.java
@@ -16,10 +16,15 @@ public class ProductListConfig {
         List<Product> productList = new ArrayList<>();
 
         productList.add(new Product("A123456", "A product number", "2020-01-01'T'12:00:00"));
+        productList.add(new Product("A789012", "A product number 2", "2020-01-01'T'12:30:00"));
         productList.add(new Product("B123456", "B product number", "2020-02-01'T'12:00:00"));
+        productList.add(new Product("B789012", "B product number 2", "2020-02-01'T'12:30:00"));
         productList.add(new Product("C123456", "C product number", "2020-03-01'T'12:00:00"));
+        productList.add(new Product("C789012", "C product number 2", "2020-03-01'T'12:30:00"));
         productList.add(new Product("D123456", "D product number", "2020-04-01'T'12:00:00"));
+        productList.add(new Product("D789012", "D product number 2", "2020-04-01'T'12:30:00"));
         productList.add(new Product("E123456", "E product number", "2020-04-01'T'12:00:00"));
+        productList.add(new Product("E789012", "E product number 2", "2020-04-01'T'12:30:00"));
 
         return productList;
     }
